45980007ae880d2dadb1d22bd2dd96d5b5255766
Prashraj78 <105579517+Prashraj78@users.noreply.github.com>
2023-10-23 10:43:08 +0530

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -7,8 +7,32 @@ import { images } from "../../constants";
 
 import "./Work.scss";
 
+const works = [
+  {
+    title: "Exeperweb (agency website)",
+    description:`
+Sole Project Development: I independently created a project at Cinoid Web, using React, Tailwind CSS, and React Redux. Framer Motion was added for engaging animations.
+
+Learning and Skill Growth: The project expanded my skills across various technologies, deepening my knowledge of React and mastery of Redux for state management.
+
+Versatile Role: Working solo, I handled diverse responsibilities, gaining proficiency in both design and functionality.
+
+Practical Application: The project provided a platform to apply theoretical knowledge practically, enhancing my learning journey.
+
+Milestone Achievement: This project marked a significant milestone in my web development journey, fueling my enthusiasm for future exploration and growth in this field.
+`,
+    techStack:
+      "Html, Css, Tailwind css,  React JS,  Git, farmer-motion",
+    tags: ["React", "All"],
+    imgUrl: images.Bookozee,
+    projectLink: "https://exeper-web.vercel.app/",
+    codeLink: "https://github.com/",
+  },
+
+];
+
 const Work = () => {
-  // const [filterWork, setFilterWork] = useState(works);
+  const [filterWork, setFilterWork] = useState(works);
   const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
@@ -19,7 +43,11 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      
+      if (item === "All") {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
     }, 500);
   };
 
@@ -30,7 +58,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["All", "React"].map((item, index) => (
+        {["All", "React", "MERN"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
@@ -48,11 +76,132 @@ const Work = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-      
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={work.imgUrl} alt={work.name} />
 
-<h2 className="head-text">
-        Working on projects <span>will be </span> upload soon
-      </h2>
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+
+                {work.codeLink ? (
+                  <a href={work.codeLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                ) : (
+                  <Tooltip
+                    title="Unfortunately, due to client confidentiality, I am unable to showcase the code !"
+                    placement="top-start"
+                  >
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </Tooltip>
+                )}
+              </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>{" "}
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
+              <p
+                className="p-text"
+                style={{ marginTop: 13, width: "100%", fontWeight: "500" }}
+              >
+                <span
+                  style={{
+                    color: "#313BAC",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {" "}
+                  Tech Stack :
+                </span>{" "}
+                {work.techStack}
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+                className="normal_icon"
+              >
+                {" "}
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                {work.codeLink ? (
+                  <a href={work.codeLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                ) : (
+                  <Tooltip
+                    title="Unfortunately, due to client confidentiality, I am unable to showcase the code !"
+                    placement="top-start"
+                  >
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </Tooltip>
+                )}
+              </div>
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </motion.div>
     </>
   );
